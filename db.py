ed6bcbcea4a52248b6b4ad1d17213ffb7d4d04fc
William Kim <willkim0117@gmail.com>
2026-04-30 22:53:39 +1000

    DB_CONFIG updated

--- db.py
+++ db.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 load_dotenv() 
  
 DB_CONFIG = {
-    "host":     os.getenv("DB_HOST", "localhost"),
-    "port":     int(os.getenv("DB_PORT", 5432)),
-    "dbname":   os.getenv("DB_NAME", "btc_dashboard"),
-    "user":     os.getenv("DB_USER", "postgres"),
+    "host": os.getenv("DB_HOST", "localhost"),
+    "port": int(os.getenv("DB_PORT", 5432)),
+    "dbname": os.getenv("DB_NAME", "btc_dashboard"),
+    "user": os.getenv("DB_USER", "postgres"),
     "password": os.getenv("DB_PASSWORD"),
 }
  
